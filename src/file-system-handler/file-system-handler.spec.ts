acdff0b06286a797696cf9a0856b6c293ad3a642
Luv Karakoti <luv.karakoti@ryd.one>
2023-11-19 03:34:16 +0100

    Updating ES version and packages

--- src/file-system-handler/file-system-handler.spec.ts
+++ src/file-system-handler/file-system-handler.spec.ts
@@ -1,5 +1,10 @@
 import { expect } from 'chai';
+import { fileURLToPath } from 'url';
+import * as path from 'path';
 import { FileSystemHandler } from './file-system-handler';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 describe('File system handler', () => {
   const fileSystemHandler = new FileSystemHandler();
@@ -9,6 +14,14 @@ describe('File system handler', () => {
       expect(
         fileSystemHandler.fileExists('random/path/to/a/non-existant/file')
       ).to.equal(false);
+    });
+
+    it('Should return false if the file does not exist', () => {
+      expect(
+        fileSystemHandler.fileExists(
+          path.join(__dirname, 'file-system-handler.ts')
+        )
+      ).to.equal(true);
     });
   });
 });
